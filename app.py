2771bec67a76c0fb84e5040327469eb75b6e16d5
hsallrounder <hsallrounder@gmail.com>
2024-02-16 23:05:32 +0530

    done

---
+++ app.py
@@ -0,0 +1,132 @@
+from flask import Flask, render_template, request, redirect, url_for
+import firebase_admin
+from firebase_admin import credentials, auth, db
+from firebase_admin.auth import InvalidIdTokenError
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from datetime import datetime
+
+app = Flask(__name__)
+
+database_url = "https://maitri-milaan-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+cred = credentials.Certificate("maitri-milaan-firebase.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': database_url
+})
+ref = db.reference('user_data')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+def find_Matches(email):
+    user_data = ref.child(email.replace('.', ',')).get()
+    df = pd.DataFrame.from_dict(ref.get(), orient='index')
+    df.reset_index(drop=True, inplace=True)
+
+    df['dob'] = pd.to_datetime(df['dob'])
+
+    current_date = datetime.now()
+    
+    age_timedelta = current_date - df['dob']
+    age_years = age_timedelta.dt.days // 365
+
+    df['age'] = age_years
+    df.drop(['dob'], axis=1, inplace=True)
+
+    age_range = [user_data['min_age'], user_data['max_age']]
+    filtered_data = df[(df['age'] >= age_range[0]) & 
+                       (df['age'] <= age_range[1]) & 
+                       (df['gender'] == user_data['gender_preference'])]
+
+    
+    # Vectorize interests
+    vectorizer = CountVectorizer()
+    interests_matrix = vectorizer.fit_transform(filtered_data['interests'].apply(lambda x: ' '.join(x)))
+    user_interests_vector = vectorizer.transform([', '.join(user_data['interests'])])
+
+    # Calculate cosine similarity
+    similarities = cosine_similarity(interests_matrix, user_interests_vector)
+
+    # Add cosine similarity to DataFrame
+    filtered_data['cosine_similarity'] = similarities.flatten()
+
+    # Sort by cosine similarity in descending order
+    filtered_data.sort_values(by='cosine_similarity', ascending=False, inplace=True)
+
+    # Find the row corresponding to the user's email
+    user_row = df[df['email'] == email].iloc[0]
+    df.drop(df[df['email'] == email].index, inplace=True)
+
+    return filtered_data, user_row
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    email = request.args.get('data')
+    preferred_matches_df, user_row = find_Matches(email)
+    
+    user_data = user_row.to_dict()
+
+    preferred_matches_df = preferred_matches_df[preferred_matches_df['email'] != email]
+    
+    preferred_matches = preferred_matches_df.to_dict('records')
+    
+    return render_template('dashboard.html', user_data=user_data, preferred_matches=preferred_matches)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        tmp = ref.child(email.replace('.', ',')).get()
+        if tmp is None:
+            return 'Email doesn\'t exists. Please choose a different email or signup.'
+        else:
+            if tmp['password'] != password:
+                return "Wrong Password."
+            return redirect(url_for('dashboard', data=email))
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        name = request.form['name']
+        password = request.form['password']
+        dob = request.form['dob']
+        gender = request.form['gender']
+        country = request.form['country']
+        state = request.form['state']
+        city = request.form['city']
+        interests = request.form.getlist('interest')
+        gender_preference = request.form['gender_preference']
+        min_age = int(request.form['min_age'])
+        max_age = int(request.form['max_age'])
+        tmp = ref.child(email.replace('.', ',')).get()
+        if tmp is not None:
+            return 'Email already exists. Please choose a different email or login.'
+        else:
+            data = {
+                "email": email,
+                "name": name,
+                "password": password,
+                "dob": dob,
+                "gender": gender,
+                "country": country,
+                "state": state,
+                "city": city,
+                "interests": interests,
+                "gender_preference": gender_preference,
+                "min_age": min_age,
+                "max_age": max_age
+            }
+            ref.child(email.replace('.', ',')).set(data)
+            return redirect(url_for('login'))
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
